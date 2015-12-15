2ad138a939545347acefe2d846fc32a49b32bc6d
cecois <cecmcgee@gmail.com>
2015-12-15 17:00:09 -0500

    got the dom and the post-content div

--- api/postfetcher.php
+++ api/postfetcher.php
@@ -6,18 +6,30 @@ $post = file_get_contents('../'.$slug);
 
 $DOM = new DOMDocument;
 libxml_use_internal_errors(true);
-   $DOM->loadHTMLFile($post);
+   $DOM->loadHTMLFile('../'.$slug);
 
    // var_dump($DOM);die();
 
    //get all H1
-   $body = $DOM->getElementsByTagName('body')->item(0);
+   $bodies = $DOM->getElementsByTagName('body');
+
+$finder = new DomXPath($DOM);
+$classname="post-content";
+$nodes = $finder->query("//*[contains(@class, '$classname')]");
+
+
 
    // $img = $dom->getElementsByTagName('img')->item(0);
 // echo $img->attributes->getNamedItem("src")->value;
 
 libxml_use_internal_errors(false);
 
-echo $body;
+// foreach ($bodies as $body) {
+// 	echo $body->nodeValue, PHP_EOL;
+// }
+// 
+foreach ($nodes as $node) {
+	echo $node->nodeValue, PHP_EOL;
+}
 
 ?>

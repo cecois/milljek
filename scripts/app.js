ef8e8ff0b976fb8d29f6e08b75c28fdd8c4c8451
cecois <cecmcgee@gmail.com>
2016-03-07 07:27:15 -0500

    a mess

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,30 @@
-$(document).ready(function(){
-        $('article>.conainer').click(function () {
-            $(this).load('about/2016/02/12/about-awards-and-recognition.html');
-        });
-    });
+---
+---
+
+/*
+----------------------------------------------------------------------  GLOBALS
+*/
+window.verbose=true
+
+if(verbose==true){console.log("appjs in da house");}
+
+{% capture posts %}
+[
+{% for post in site.posts %}
+{
+"title"    : "{{ post.title }}",
+"url"      : "{{ post.url }}",
+"slug"      : "{{ post.slug }}",
+"date"     : "{{ post.date | date: "%B %d, %Y" }}",
+"content"  : "{{ post.content | escape }}"
+} {% if forloop.last %}{% else %},{% endif %}
+{% endfor %}
+]
+{% endcapture %}
+
+
+var appState = new State({});
+
+var posts = {{posts | strip_newlines}}
+cxPosts = new PostsCollection(posts, {});
+vPostsView = new PostsView(cxPosts, {});
